a478a5ed511cd8d6219ee4ebf596dd2da7248d0e
Prayuda Sagita Wardana <wpruda@gmail.com>
2022-06-02 22:26:32 +0700

    tugas 4 fungsi algo

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // ! JANGAN DIMODIFIKASI
 const dates = [
-  "2019-01-02", // 1546387200 (epoch time, dalam detik)
-  "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
-  "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
-  "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
-  "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
+  '2019-01-02', // 1546387200 (epoch time, dalam detik)
+  '2022-05-06 00:00:00 UTC-2', // 1651802400 (epoch time, dalam detik)
+  '2021-03-04 07:00:00 UTC', // 1614841200 (epoch time, dalam detik)
+  '2021-04-05 00:00:00 UTC+2', // 1617573600 (epoch time, dalam detik)
+  '2020-02-03 UTC+7', // 1580662800 (epoch time, dalam detik)
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (dates, list) => {
+  let arrTanggal = [];
+  for (i = 0; i < dates.length; i++) {
+    arrTanggal.push(Date.parse(dates[i]) / 1000);
+  }
+  if (isNaN(list)) {
+    arrTanggal.sort();
+    for (i = 0; i < arrTanggal.length; i++) {
+      arrTanggal[i].toFixed(3);
+    }
+    return arrTanggal.join('-');
+  } else {
+    return arrTanggal[list].toString();
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
